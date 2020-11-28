4574e57bda943799b9ddff553c2e633c011021ca
kovalenko-dmytro <kovalenko.dm80@gmail.com>
2020-11-28 20:11:11 +0200

    start upload remote resource

--- src/main/java/com/example/testresultinvoker/service/impl/UploadTestResultServiceImpl.java
+++ src/main/java/com/example/testresultinvoker/service/impl/UploadTestResultServiceImpl.java
@@ -1,13 +1,27 @@
 package com.example.testresultinvoker.service.impl;
 
 import com.example.testresultinvoker.service.UploadTestResultService;
+import jcifs.smb.NtlmPasswordAuthentication;
+import jcifs.smb.SmbFile;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 @Service
 public class UploadTestResultServiceImpl implements UploadTestResultService {
 
     @Override
     public void upload(String path, String login, String password) {
-        String pathToDir = path;
+        NtlmPasswordAuthentication auth = new NtlmPasswordAuthentication("", login, password);
+        InputStream in;
+        try {
+            SmbFile smbFile = new SmbFile(path, auth);
+            if (smbFile.exists()) {
+                in = smbFile.getInputStream();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
